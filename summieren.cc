edaca198cb5a3972b111b60180481a3b1b372cd4
MistySorrow <>
2021-10-20 13:46:57 +0000

    Aufgabe 2

--- summieren.cc
+++ summieren.cc
@@ -7,13 +7,13 @@ int main() {
   int zahl, zahl2;
   fin >> zahl >> zahl2;
   fin.close();
-  std::cout << zahl << " " << zahl2 << std::endl;
+  //std::cout << zahl << " " << zahl2 << std::endl;
 
   fin.open("daten.txt");
   std::ofstream fout("datensumme.txt");
   for(int i = 1; i < 235; i++) {
     fin >> zahl >> zahl2;
-    std::cout << zahl << " " << zahl2 << std::endl;
+    //std::cout << zahl << " " << zahl2 << std::endl;
     fout << zahl + zahl2 << std::endl;
     }
   fin.close();
